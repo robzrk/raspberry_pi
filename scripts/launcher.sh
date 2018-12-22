c923b4012e52ea8251d7f336cfe97743ce523736
Rob Zirkel <robzirkel@gmail.com>
2018-12-22 16:01:04 -0600

    Started vncserver with launcher - limited launcher to start subtasks only once

--- scripts/launcher.sh
+++ scripts/launcher.sh
@@ -1,9 +1,18 @@
-#!/bin/bash
+#/bin/bash
 SCRIPTS_DIR=~/raspberry_pi/scripts
+
+# Only run this launcher once per boot!
+if [ -f /tmp/launcher_started ]; then
+    exit 
+else
+    touch /tmp/launcher_started
+fi
+
+vncserver :1
 
 $SCRIPTS_DIR/update_repo.sh
 
-sleep 5
+sleep 1 
 
 $SCRIPTS_DIR/read_email.py
 
