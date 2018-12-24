fb14214405da55634d1d1c67afbbc35a271bd851
Rob Zirkel <robzirkel@gmail.com>
2018-12-23 19:13:21 -0600

    updated launcher to show vnc IP addr

--- scripts/launcher.sh
+++ scripts/launcher.sh
@@ -7,15 +7,21 @@ if [ -f /tmp/launcher_started ]; then
 else
     touch /tmp/launcher_started
 fi
-disown -a
+#disown -a
 
 echo "running!" >> /tmp/launcher_log
+
+IPADDR=`ifconfig | grep wlan0 -A 5 | grep inet | grep -v inet6 | awk '{ print $2 }'`
+echo "Starting VNC server at ${IPADDR}:1 ..."
 vncserver :1
 
+echo "Updating repo ..."
 $SCRIPTS_DIR/update_repo.sh
 
 sleep 1 
 
+echo "Reading the lastest emails ..."
 $SCRIPTS_DIR/read_email.py
 
+echo "Launching pi_ui ..."
 lxterminal -e $SCRIPTS_DIR/pi_ui.sh
