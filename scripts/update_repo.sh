b702820572394b2ebff1c854440f759109d1d453
Rob Zirkel <robzirkel@gmail.com>
2018-12-30 19:18:26 -0600

    bugfix to update_repo

--- scripts/update_repo.sh
+++ scripts/update_repo.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 cd ~/raspberry_pi
-git stash 
-git reset --hard HEAD
+git status | grep modified >/dev/null
+LC=$?
+if [ $LC -eq 0 ]; then
+    git stash 
+    git reset --hard HEAD
+fi
 git pull
-git stash pop
+if [ $LC -eq 0 ]; then
+    git stash pop
+fi
 
 ~/raspberry_pi/scripts/post_update_setup.sh
