111db8a152014b9c9b26838c8db676030aaa5408
Rob Zirkel <robzirkel@gmail.com>
2019-03-10 15:40:15 -0500

    Added timeout to print lock function.

--- scripts/print_lib.sh
+++ scripts/print_lib.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 LOCKFILE=/tmp/print_lib.lock
+PRINT_LOCK_TO=10
+LOG_PATH=/tmp/pi_ui.log
+
+function log() {
+    local LOG_MSG=$1
+    echo "`date`: $LOG_MSG" >> $LOG_PATH
+}
 
 function acquire_print_lock() {
+    TIMER_START=$SECONDS
     while [ 1 ]; do
     	mkdir $LOCKFILE 2> /dev/null
     	if [ $? -eq 0 ]; then
     	    break
     	fi
+        DURATION=$(( SECONDS - TIMER_START ))
+        if [ $DURATION -gt $PRINT_LOCK_TO ]; then
+            log "WARNING: Timed out waiting for print lock!"
+            release_print_lock
+            # we'll acquire it on the next iteration.
+        fi
     done
 }
 
