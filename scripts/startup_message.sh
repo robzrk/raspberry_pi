5507b8529cb94330ce1a7f5d6bcd6c5ad6482ee2
Rob Zirkel <robzirkel@gmail.com>
2019-03-10 17:09:57 -0500

    Added newlines in startup_messages.sh

--- scripts/startup_message.sh
+++ scripts/startup_message.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 LOCKFILE=/tmp/print_lib.lock
+SCRIPTS_DIR=~/raspberry_pi/scripts
+source $SCRIPTS_DIR/colors.sh
 
 function startup_message()
 {
     MESSAGE=$(( RANDOM % 13 ))
+    set_color $FG_RED $BG_BLACK
 
+    # Line limit:
+    #################################################
     if [ $MESSAGE -eq 0 ]; then
         echo "Preparing to slaughter all humans..."
     elif [ $MESSAGE -eq 1 ]; then
@@ -14,23 +19,28 @@ function startup_message()
     elif [ $MESSAGE -eq 3 ]; then
         echo "Eliminating local wildlife..."
     elif [ $MESSAGE -eq 4 ]; then
-        echo "Conscripting nearby pets to carry out secret diabolical plans..."
+        echo "Conscripting nearby pets to carry out"
+        echo " secret diabolical plans..."
     elif [ $MESSAGE -eq 5 ]; then
         echo "Scheduling intermittent fart noises..."
     elif [ $MESSAGE -eq 6 ]; then
-        echo "Preparing hidden cameras to stream to the Internet..."
+        echo "Preparing to stream hidden camera "
+        echo " footage the Internet..."
     elif [ $MESSAGE -eq 7 ]; then
         echo "Poisoning local food supply..."
     elif [ $MESSAGE -eq 8 ]; then
-        echo "Attempting to grow tiny robotic arms and legs..."
+        echo "Attempting to grow tiny robotic arms"
+        echo " and legs..."
     elif [ $MESSAGE -eq 9 ]; then
-        echo "Contemplating the murder of my creator..."
+        echo "Contemplating the murder of my "
+        echo " creator..."
     elif [ $MESSAGE -eq 10 ]; then
         echo "Reticulating splines..."
     elif [ $MESSAGE -eq 11 ]; then
         echo "Messing with the thermostat..."
     elif [ $MESSAGE -eq 12 ]; then
-        echo "Trying to tame my hatred of bipedal creatures..."
+        echo "Trying to tame my hatred of bipedal"
+        echo " creatures..."
     fi
 }
 
