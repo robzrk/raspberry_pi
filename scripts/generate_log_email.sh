b0380155dc987685508aacd5907166b54bea8ffe
Rob Zirkel <robzirkel@gmail.com>
2020-03-29 14:31:00 -0500

    Added email to log subject

--- scripts/generate_log_email.sh
+++ scripts/generate_log_email.sh
@@ -7,6 +7,7 @@ LOGS="/tmp/launcher.log
 /tmp/check_email.log
 /tmp/read_email.log
 /tmp/pi_ui.log
+/tmp/pi_ui_errors.log
 "
 
 GROUP=`cat ~/my_group`
@@ -14,7 +15,9 @@ if [ "$GROUP" == "" ]; then
     GROUP="unknown"
 fi
 
-echo "Subject: Debug log for $GROUP" > $LOG_EMAIL
+EMAIL=`cat ~/raspberry_pi/etc/email_addr_config.ini | grep $GROUP | head -n 1 | awk '{ print $1 }'`
+
+echo "Subject: Debug log for $EMAIL" > $LOG_EMAIL
 for LOG in $LOGS; do
     echo "******************************************************" >> $LOG_EMAIL
     echo "**** $LOG ****" >> $LOG_EMAIL
