a61dce3e1e921402678984a129ab284da8ebb165
robzrk <robzirkel@gmail.com>
2021-02-14 10:20:53 -0600

    Update update_repo.sh

--- scripts/update_repo.sh
+++ scripts/update_repo.sh
@@ -3,6 +3,7 @@ LOG_PATH="/tmp/update_repo.log"
 
 cd ~/raspberry_pi
 
+echo "Updating source repo..." | tee -a $LOG_PATH
 git fetch origin master | tee -a $LOG_PATH
 git reset --hard FETCH_HEAD | tee -a $LOG_PATH
 
